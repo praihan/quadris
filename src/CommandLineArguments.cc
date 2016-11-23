24fcddd60e56a7ee8deda5bcdd2070affff97634
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-23 06:58:54 -0500

    Implement CommandLineArguments

--- src/CommandLineArguments.cc
+++ src/CommandLineArguments.cc
@@ -1,1 +1,79 @@
 #include "CommandLineArguments.h"
+
+#include <string>
+#include <stdexcept>
+#include <unordered_map>
+#include <functional>
+#include <cassert>
+
+namespace {
+  bool tryParseInt(int& outVal, const std::string& str) {
+    try {
+      outVal = std::stoi(str);
+      return true;
+    } catch (const std::invalid_argument&) {
+    } catch (const std::out_of_range&) {
+    }
+    return false;
+  }
+
+  constexpr char ARG_TEXT[] = "-text";
+  constexpr char ARG_SEED[] = "-seed";
+  constexpr char ARG_SCRIPTFILE[] = "-scriptfile";
+  constexpr char ARG_STARTLEVEL[] = "-startlevel";
+}
+
+qd::CommandLineArguments::CommandLineArguments(const int argc, char* argv[]) {
+  int i = 1;
+
+  auto getNextArg = [argc, argv, &i](const std::string& argName) -> std::string {
+    if ((i + 1) >= argc) {
+      throw std::runtime_error{"Expected value for argument: " + argName};
+    }
+    return argv[i + 1];
+  };
+
+  // inefficient, but okay since we only read the arguments once
+  std::unordered_map<std::string, std::function<void()>> argument_actions;
+
+  argument_actions[ARG_TEXT] = [this]() {
+    this->text = std::make_unique<bool>(true);
+  };
+
+  argument_actions[ARG_SEED] = [this, &i, &getNextArg]() {
+    int seed;
+    if (!tryParseInt(seed, getNextArg(ARG_SEED))) {
+      throw std::runtime_error{ARG_SEED};
+    }
+    this->seed = std::make_unique<int>(seed);
+    ++i;
+  };
+
+  argument_actions[ARG_SCRIPTFILE] = [this, &i, &getNextArg]() {
+    std::string scriptFile = getNextArg(ARG_SCRIPTFILE);
+    this->scriptFile = std::make_unique<std::string>(std::move(scriptFile));
+    ++i;
+  };
+
+  argument_actions[ARG_STARTLEVEL] = [this, &i, &getNextArg]() {
+    int startLevel;
+    if (!tryParseInt(startLevel, getNextArg(ARG_STARTLEVEL))) {
+      throw std::runtime_error{ARG_STARTLEVEL};
+    }
+    this->startLevel = std::make_unique<int>(startLevel);
+    ++i;
+  };
+  
+  for (; i < argc; ++i) {
+    // std::string_view would be nice
+    std::string arg = argv[i];
+    auto action = argument_actions.find(arg);
+
+    if (action != argument_actions.end()) {
+      (action->second)();
+    }
+    else {
+      throw std::runtime_error{"Unknown argument name: " + arg};
+    }
+  }
+}
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,12 @@
 #include <iostream>
 
-int main() {
+#include "CommandLineArguments.h"
+
+int main(int argc, char* argv[]) {
+  qd::CommandLineArguments cmdLineArgs{ argc, argv };
+
+  std::cout << "Seed: " << (cmdLineArgs.seed ? std::to_string(*cmdLineArgs.seed) : "<none>") << std::endl;
+  std::cout << "Start Level: " << (cmdLineArgs.startLevel ? std::to_string(*cmdLineArgs.startLevel) : "<none>") << std::endl;
+  std::cout << "Text: " << (cmdLineArgs.text ? (*cmdLineArgs.text ? "true" : "false") : "<none>") << std::endl;
+  std::cout << "Start Level: " << (cmdLineArgs.scriptFile ? *cmdLineArgs.scriptFile : "<none>") << std::endl;
 }
