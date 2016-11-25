d6e7ce9db38017c4ee3d0430a9ba84ad7236e2ea
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-11-24 22:07:57 -0500

    Add X11.h header

---
+++ src/X11.h
@@ -0,0 +1,14 @@
+#ifndef QDX11_H_
+#define QDX11_H_
+
+// Note for some reason, this doesn't work with GCC
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic ignored "-Wvariadic-macros"
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+
+// #pragma GCC diagnostic pop
+
+#endif // QDX11_H_
