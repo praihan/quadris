0c0f0042be99a5e80490c85b10dc3651c535d893
robin-pan <pan923pan@gmail.com>
2016-11-30 23:43:22 -0500

    Made blocks heavy in Level 3 and 4

--- src/Levels/Level3.cc
+++ src/Levels/Level3.cc
@@ -33,6 +33,16 @@ namespace qd {
   }
 
   void Level3::executeCommand(const Command& command) {
-
+    BaseLevel::executeCommand(command);
+    
+    if (command.type() == Command::Type::LEFT || command.type() == Command::Type::RIGHT || command.type() == Command::Type::DOWN || 
+        command.type() == Command::Type::CLOCKWISE || command.type() == Command::Type::COUNTER_CLOCKWISE) {
+      Block &activeBlock = _board.activeBlock();
+      
+      if (_canMove(activeBlock, BaseLevel::Direction::DOWN)) {
+        activeBlock.position.row += 1;
+        _board.cellsUpdated().notifyObservers(_board.cells());
+      }
+    }
   }
 }
--- src/Levels/Level4.cc
+++ src/Levels/Level4.cc
@@ -12,6 +12,16 @@ namespace qd {
   }
 
   void Level4::executeCommand(const Command& command) {
-
+    BaseLevel::executeCommand(command);
+    
+    if (command.type() == Command::Type::LEFT || command.type() == Command::Type::RIGHT || command.type() == Command::Type::DOWN || 
+        command.type() == Command::Type::CLOCKWISE || command.type() == Command::Type::COUNTER_CLOCKWISE) {
+      Block &activeBlock = _board.activeBlock();
+      
+      if (_canMove(activeBlock, BaseLevel::Direction::DOWN)) {
+        activeBlock.position.row += 1;
+        _board.cellsUpdated().notifyObservers(_board.cells());
+      }
+    }
   }
 }
