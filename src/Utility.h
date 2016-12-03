c00efe09b48d27b0b32eec6f20ee4f5ee65840e8
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-12-03 00:14:42 -0500

    Oops

--- src/Utility.h
+++ src/Utility.h
@@ -6,6 +6,30 @@
 #include <string>
 
 namespace qd {
+  template <class T, class U, template <class...> class Checker>
+  struct NoExpressionsOf1stValid {
+    static constexpr bool value = (
+      !Checker<T&, U>::value &&
+      !Checker<const T&, U>::value &&
+      !Checker<T&&, U>::value &&
+      !Checker<const T&&, U>::value
+    );
+    // static_assert(!Checker<T&, U>::value, "");
+    // static_assert(!Checker<const T&, U>::value, "");
+    // static_assert(!Checker<T&&, U>::value, "");
+    // static_assert(!Checker<const T&&, U>::value, "");
+  };
+
+  template <class T, class U, template <class...> class Checker>
+  struct NoExpressionsOf2ndValid {
+    static constexpr bool value = (
+      !Checker<T, U&>::value &&
+      !Checker<T, const U&>::value &&
+      !Checker<T, U&&>::value &&
+      !Checker<T, const U&&>::value
+    );
+  };
+
   template <class F>
   decltype(auto) iife(F&& f) noexcept(noexcept(std::forward<F>(f)())) {
     return std::forward<F>(f)();
