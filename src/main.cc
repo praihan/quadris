893d448963e24b460d9b28f3209e7d36bb5b31e3
Pranjal Raihan <pranjal@pranjal-raihan.com>
2016-12-05 14:44:44 -0500

    Remove debug stuff

--- src/main.cc
+++ src/main.cc
@@ -26,11 +26,6 @@ auto makeLevelFactory() {
 int main(int argc, char* argv[]) {
   qd::CommandLineArguments cmdLineArgs{ argc, argv };
 
-  std::cout << "Seed: " << (cmdLineArgs.seed.hasValue() ? std::to_string(*cmdLineArgs.seed) : "<none>") << std::endl;
-  std::cout << "Start Level: " << (cmdLineArgs.startLevel.hasValue() ? std::to_string(*cmdLineArgs.startLevel) : "<none>") << std::endl;
-  std::cout << "Text: " << (cmdLineArgs.text.hasValue() ? (*cmdLineArgs.text ? "true" : "false") : "<none>") << std::endl;
-  std::cout << "Script File: " << (cmdLineArgs.scriptFile.hasValue() ? *cmdLineArgs.scriptFile : "<none>") << std::endl;
-
   if (cmdLineArgs.scriptFile.hasValue()) {
     qd::Level0::sequenceFileName = *cmdLineArgs.scriptFile;
   }
